3a7c5bde808fc59a1e917201f8afe082f92bb395
Deb <debkbanerji@gmail.com>
2020-10-08 23:38:48 -0700

    Update server

--- code/piano-server.py
+++ code/piano-server.py
@@ -18,12 +18,16 @@ sampler.start()
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 server_address = (SERVER_LAN_ADDRESS_STRING_HERE, 11111)
-print('Listing on {}, port {}'.format(server_address[0], server_address[1]))
+print('Listening on {}, port {}'.format(server_address[0], server_address[1]))
 sock.bind(server_address)
 
+old_pressed_keys = None
 while True:
 
     data, address = sock.recvfrom(1024) # 1024 is arbitrarily chosen as packet size? - idk if that's what it is, but it works
     pressed_keys = np.frombuffer(data, dtype='short')
 
-    sampler.update_optimized_v2(pressed_keys.copy())
+    if old_pressed_keys is None or not np.array_equal(old_pressed_keys, pressed_keys):
+        print(pressed_keys)
+        sampler.update_optimized_v2(pressed_keys.copy())
+    old_pressed_keys = pressed_keys
