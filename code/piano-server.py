9fe909a26fefb674b3ad20b8521fd7ffe68d5ae2
Deb <debkbanerji@gmail.com>
2020-10-09 22:12:09 -0700

    Non blocking sockets

--- code/piano-server.py
+++ code/piano-server.py
@@ -16,18 +16,27 @@ sampler.start()
 
 # set up UDP server
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.setblocking(0)
 
 server_address = (SERVER_LAN_ADDRESS_STRING_HERE, 11111)
 print('Listening on {}, port {}'.format(server_address[0], server_address[1]))
 sock.bind(server_address)
 
 old_pressed_keys = None
-while True:
-
-    data, address = sock.recvfrom(1024) # 1024 is arbitrarily chosen as packet size? - idk if that's what it is, but it works
-    pressed_keys = np.frombuffer(data, dtype='short')
-
-    if old_pressed_keys is None or not np.array_equal(old_pressed_keys, pressed_keys):
-        print(pressed_keys)
-        sampler.update_optimized_v2(pressed_keys.copy())
-    old_pressed_keys = pressed_keys
+pressed_keys = np.zeros(25, dtype=np.uintc)
+try:
+    while True:
+        try:
+            data, _address = sock.recvfrom(1024) # 1024 is arbitrarily chosen as packet size? - idk if that's what it is, but it works
+        except Exception as e:
+            sampler.update_optimized_v2(pressed_keys.copy())
+        else:
+        # print(sys.getsizeof(data))
+            pressed_keys = np.frombuffer(data, dtype=np.uintc)
+            print(pressed_keys)
+            # if old_pressed_keys is None or not np.array_equal(old_pressed_keys, pressed_keys):
+            #     print(pressed_keys)
+            sampler.update_optimized_v2(pressed_keys.copy())
+            old_pressed_keys = pressed_keys
+except KeyboardInterrupt:
+    sampler.close()
