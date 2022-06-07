09f0ca821d06967369b640bb475656148a3d5edd
Deb <debkbanerji@gmail.com>
2022-06-06 21:26:40 -0700

    Timestamp Based Buffer

--- code/piano-flask-server.py
+++ code/piano-flask-server.py
@@ -3,13 +3,17 @@ from queue import Queue
 import json
 import numpy as np
 import threading
+import time
 import RPi.GPIO as GPIO # Import Raspberry Pi GPIO library
 
 import logging
 logging.getLogger('werkzeug').setLevel(logging.ERROR)
 
 MAX_BUFFER_SIZE = 50 # add a little memory for if a key was pressed - tweak this if things seem off
+BUFFER_MAX_TIME_NS  = 500 * 1000 * 1000 # 0.5 seconds
+
 buffer = Queue(maxsize=MAX_BUFFER_SIZE)
+buffer_timestamps = Queue(maxsize=MAX_BUFFER_SIZE)
 
 NUM_KEYS = 26  # 25 keys + pedal
 key_numbers = [*range(0, NUM_KEYS)]
@@ -52,9 +56,18 @@ class Pin_Listener_Thread(threading.Thread):
                 pressed_map[key] = GPIO.input(key + key_offset) == GPIO.LOW
             pressed_keys = np.array([1 if pressed_map[key] else 0 for key in key_numbers], dtype=np.uintc)
 
+            input_time = time.time_ns()
             if buffer.full():
                 buffer.get()
-            buffer.put(pressed_keys)
+                buffer_timestamps.get()
+
+            while not buffer.empty() and input_time - buffer_timestamps.queue[0] > BUFFER_MAX_TIME_NS:
+                buffer.get()
+                buffer_timestamps.get()
+
+            if buffer.empty() or not np.array_equal(buffer.queue[-1], pressed_keys):
+                buffer.put(pressed_keys)
+                buffer_timestamps.put(input_time)
 
 
 if __name__ == '__main__':
