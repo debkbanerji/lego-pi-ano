1db0e302e41b087f7b09fe0981e7bf8214eaf382
Deb <debkbanerji@gmail.com>
2022-06-07 16:53:28 -0700

    Play Sounds

--- code/static/index.js
+++ code/static/index.js
@@ -2,19 +2,42 @@ const FETCH_INTERVAL_MS = 50;
 
 let isRequestInFlight = false;
 
+let pressedKeys = [];
+const sounds = Array(30) // Expect at most 30 keys
+    .fill(0)
+    .map(
+        (_, i) =>
+            new Howl({
+                src: [
+                    `assets/key-sounds/${(i + 1).toLocaleString("en-US", {
+                        minimumIntegerDigits: 2,
+                        useGrouping: false
+                    })}.wav`
+                ]
+            })
+    );
+
 setInterval(() => {
     if (!isRequestInFlight) {
         fetch(`${window.location.origin}/keys`)
             .then(response => response.json())
-            .then(keyArray => {
-                console.log(keyArray);
-                const displayKeys = keyArray.map(key => (key ? "■" : "□"));
+            .then(data => {
+                const displayKeys = data.map(key => (key ? "■" : "□"));
                 const displayKeysString = displayKeys.join(" ");
-                // const displayKeysLabelString = Object.keys(displayKeys).join(" ");
                 document.getElementById("no-signals-text").hidden = true;
                 document.getElementById(
                     "debug-text"
                 ).innerHTML = displayKeysString;
+
+                const oldPressedKeys = pressedKeys;
+                pressedKeys = data;
+
+                Object.keys(pressedKeys).forEach(keyIndex => {
+                    if (pressedKeys[keyIndex] && !oldPressedKeys[keyIndex]) {
+                        sounds[keyIndex].play();
+                    }
+                });
+
                 isRequestInFlight = false;
             })
             .catch(function() {
