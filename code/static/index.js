419c270cbd38c9831ada5505db473d249e9ad394
Deb <debkbanerji@gmail.com>
2022-06-11 23:00:17 -0700

    mp3 Sounds

--- code/static/index.js
+++ code/static/index.js
@@ -7,112 +7,108 @@ let isRequestInFlight = false;
 let pressedKeys = [];
 
 let pinMap = Array(MAX_KEY_COUNT)
-    .fill(0)
-    .map((_, i) => i);
+  .fill(0)
+  .map((_, i) => i);
 const sounds = Array(MAX_KEY_COUNT)
-    .fill(0)
-    .map((_, i) => {
-        return new Howl({
-            src: [
-                `assets/key-sounds/${(i + 1).toLocaleString("en-US", {
-                    minimumIntegerDigits: 2,
-                    useGrouping: false
-                })}.wav`
-            ]
-        });
+  .fill(0)
+  .map((_, i) => {
+    return new Howl({
+      src: [`assets/key-sounds/${i + 1}.mp3`],
     });
+  });
 
 try {
-    const urlParams = new URLSearchParams(window.location.search);
-    newPinMap = JSON.parse(urlParams.get("pinMap"));
-    newPinMap.forEach((pin, key) => {
-        pinMap[key] = pin;
-    });
+  const urlParams = new URLSearchParams(window.location.search);
+  newPinMap = JSON.parse(urlParams.get("pinMap"));
+  newPinMap.forEach((pin, key) => {
+    pinMap[key] = pin;
+  });
 } catch (e) {
-    // do nothing
+  // do nothing
 }
 
 function onTapPage() {
-    isSoundEnabled = true;
-    keysTarget.innerHTML = "Waiting for Signal/Loading in audio files...";
+  isSoundEnabled = true;
+  keysTarget.innerHTML = "Waiting for Signal/Loading in audio files...";
 }
 document.addEventListener("click", onTapPage);
 document.addEventListener("touchstart", onTapPage);
 
 setInterval(() => {
-    if (!isRequestInFlight) {
-        fetch(`${window.location.origin}/keys`)
-            .then(response => response.json())
-            .then(data => {
-                if (isSoundEnabled) {
-                    const oldPressedKeys = pressedKeys;
-                    pressedKeys = [];
+  if (!isRequestInFlight) {
+    fetch(`${window.location.origin}/keys`)
+      .then((response) => response.json())
+      .then((data) => {
+        if (isSoundEnabled) {
+          const oldPressedKeys = pressedKeys;
+          pressedKeys = [];
 
-                    data.forEach((pressed, pin) => {
-                        pressedKeys[pinMap[pin]] = pressed || 0;
-                    });
+          data.forEach((pressed, pin) => {
+            pressedKeys[pinMap[pin]] = pressed || 0;
+          });
 
-                    keysTarget.innerHTML = "";
+          keysTarget.innerHTML = "";
 
-                    Object.keys(pressedKeys).forEach(keyIndex => {
-                        if (
-                            pressedKeys[keyIndex] &&
-                            (oldPressedKeys[keyIndex] == null ||
-                                oldPressedKeys[keyIndex] === 0)
-                        ) {
-                            sounds[keyIndex].play();
-                        }
+          Object.keys(pressedKeys).forEach((keyIndex) => {
+            if (
+              pressedKeys[keyIndex] &&
+              (oldPressedKeys[keyIndex] == null ||
+                oldPressedKeys[keyIndex] === 0)
+            ) {
+              sounds[keyIndex].play();
+            }
 
-                        const key = document.createElement("div");
-                        key.className = `display-key${
-                            pressedKeys[keyIndex] ? " display-key-pressed" : ""
-                        }`;
-                        keysTarget.appendChild(key);
-                    });
-                }
-                isRequestInFlight = false;
-            })
-            .catch(function() {
-                keysTarget.innerHTML = "No signal received yet";
-                isRequestInFlight = false;
-            });
-        isRequestInFlight = true;
-    }
+            const key = document.createElement("div");
+            key.className = `display-key${
+              pressedKeys[keyIndex] ? " display-key-pressed" : ""
+            }`;
+            keysTarget.appendChild(key);
+          });
+        }
+        isRequestInFlight = false;
+      })
+      .catch(function () {
+        keysTarget.innerHTML = "No signal received yet";
+        isRequestInFlight = false;
+      });
+    isRequestInFlight = true;
+  }
 }, FETCH_INTERVAL_MS);
 
 const pinMapControls = document.getElementById("pin-map-controls");
 document
-    .getElementById("toggle-pin-map-controls")
-    .addEventListener("click", () => {
-        pinMapControls.hidden = !pinMapControls.hidden;
-    });
+  .getElementById("toggle-pin-map-controls")
+  .addEventListener("click", () => {
+    pinMapControls.hidden = !pinMapControls.hidden;
+  });
 
 Array(MAX_KEY_COUNT)
-    .fill(0)
-    .forEach((_, i) => {
-        const container = document.createElement("div");
-        container.innerHTML = `
+  .fill(0)
+  .forEach((_, i) => {
+    const container = document.createElement("div");
+    container.innerHTML = `
         <div>
-        <b>Pin ${i} Key: </b>  <input type="number" id="key-${i}" name="key-${i}" step="1" min="0" max="${MAX_KEY_COUNT -
-            1}" value="${pinMap[i] || i}">
+        <b>Pin ${i} Key: </b>  <input type="number" id="key-${i}" name="key-${i}" step="1" min="0" max="${
+      MAX_KEY_COUNT - 1
+    }" value="${pinMap[i] || i}">
         </div>
       `;
-        pinMapControls.appendChild(container);
-    });
+    pinMapControls.appendChild(container);
+  });
 
-[...pinMapControls.children].forEach(container => {
-    container.children[0].children[1].addEventListener("change", val => {
-        const newPinMap = [];
-        [...pinMapControls.children].forEach((container, i) => {
-            newPinMap[i] = Number(container.children[0].children[1].value);
-        });
-        pinMap = newPinMap;
-        history.pushState(
-            {},
-            "Lego Pi-ano",
-            `${window.location.origin}?pinMap=${JSON.stringify(pinMap)}`
-        );
+[...pinMapControls.children].forEach((container) => {
+  container.children[0].children[1].addEventListener("change", (val) => {
+    const newPinMap = [];
+    [...pinMapControls.children].forEach((container, i) => {
+      newPinMap[i] = Number(container.children[0].children[1].value);
     });
+    pinMap = newPinMap;
+    history.pushState(
+      {},
+      "Lego Pi-ano",
+      `${window.location.origin}?pinMap=${JSON.stringify(pinMap)}`
+    );
+  });
 });
 
 // TODO: Write to pin map and URL on value change
